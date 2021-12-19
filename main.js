b3de204ef55c6d33ae45f9a8be022aca856ff84d
sangyeop0529 <sangyeop0529@gmail.com>
2021-12-19 16:34:45 +0900

    JS : Navbar

--- main.js
+++ main.js
@@ -4,11 +4,26 @@
 const navbar = document.querySelector('#navbar')
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(`navbarHeight: ${navbarHeight}`);
+  // console.log(window.scrollY);
+  // console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+// data- 필요
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  // console.log(event.target.dataset.link);
+  const srollTo = document.querySelector(link);
+  srollTo.scrollIntoView({behavior: "smooth"});
 });

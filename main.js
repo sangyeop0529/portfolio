3a15d4a26ffd861e4ae70c8a16eda29ae183932e
sangyeop0529 <sangyeop0529@gmail.com>
2021-12-20 01:23:24 +0900

    Final touch

--- main.js
+++ main.js
@@ -104,9 +104,67 @@ workBtnContainer.addEventListener('click', e => {
   // for(let project of projects) {}
 });
 
+// 난이도 상***
+// 1. 모든 섹션 요소들을과 메뉴아이템을 가지고 옴
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킴
+
+// no. 1
+const sectionIds = [
+  "#home", 
+  "#about", 
+  "#skills", 
+  "#work", 
+  "#testimonials", 
+  "#contact"
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+// no. 2
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+
+function selectNavitem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+}
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({behavior: "smooth"});
+  selectNavitem(navItems[sectionIds.indexOf(selector)]);
 }
 
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.3,
+}
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if(!entry.inIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if(entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+
+    }
+  });
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+  if(window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (window.scrollY + window.innerHeight === document.body.clientHeight) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavitem(navItems[selectedNavIndex]);
+});
